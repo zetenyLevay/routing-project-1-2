4c9992e4b15e752de8a37e671bf03ade394f4ddb
Zeteny Levay <levay.zeteny3@gmail.com>
2025-03-31 13:04:40 +0200

    Created reading method in POIReader
    Former-commit-id: 9093ffc8b9278416d89dd786cdcf54228455a0a4

--- src/main/java/ClosureAnalysis/Data/Readers/RoadSegmentReader.java
+++ src/main/java/ClosureAnalysis/Data/Readers/RoadSegmentReader.java
@@ -1,6 +1,14 @@
 package ClosureAnalysis.Data.Readers;
 
 
+import ClosureAnalysis.Data.Enums.POIType;
+import ClosureAnalysis.Data.Models.Coordinate;
+import ClosureAnalysis.Data.Models.PointOfInterest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 
 public class RoadSegmentReader extends Reader {
@@ -10,8 +18,6 @@ public class RoadSegmentReader extends Reader {
 
     @Override
     protected void readFile() throws IOException {
-
-
     }
 
     @Override
@@ -20,7 +26,7 @@ public class RoadSegmentReader extends Reader {
     }
 
     public static void main(String[] args) throws IOException {
-        Reader reader = new RoadSegmentReader("data/ClosureAnalysis/RoadUsage/ReprojectedRoadData.geojson");
+        Reader reader = new RoadSegmentReader("data/ClosureAnalysis/POI_data.geojson");
         reader.readFile();
     }
 }
