416f8b395ea05f83d4d40692bfcb3feee9280316
Nurali Kabiyev <n.kabiyev@student.maastrichtuniversity.nl>
2025-04-01 00:38:59 +0200

    Added content to mainUI so it will show map
    Former-commit-id: a2ca20fc995d7132932fea5a442143e01887cc67

--- src/main/java/gui/mainUI.java
+++ src/main/java/gui/mainUI.java
@@ -1,8 +1,24 @@
 package gui;
 
-import org.jxmapviewer.*;  
-import org.jxmapviewer.viewer.*;  
+import org.jxmapviewer.*;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactory;
+import javax.swing.*;
 
 public class mainUI {
+    public static void main(String[] args) {  
+        JXMapViewer mapViewer = new JXMapViewer();
+        TileFactory tileFactory = new DefaultTileFactory(new OSMTileFactoryInfo());;
+        mapViewer.setTileFactory(tileFactory);
+        mapViewer.setZoom(5);
+        mapViewer.setAddressLocation(new GeoPosition(47.4979, 19.0402));
 
+
+        JFrame frame = new JFrame("Map Viewer");
+        frame.getContentPane().add(mapViewer);
+        frame.setSize(800, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
