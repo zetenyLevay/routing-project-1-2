d07187d4c4258f031499d3b49feab493815bdbbc
Zeteny Levay <levay.zeteny3@gmail.com>
2025-04-14 17:49:46 +0200

    created NearbyPOIs object, updated PoiFinder
    Former-commit-id: 4029f5c2b4681e477bac71d43678e62d0ce73d5e

---
+++ src/main/java/ClosureAnalysis/Data/Models/NearbyPOIs.java
@@ -0,0 +1,30 @@
+package ClosureAnalysis.Data.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NearbyPOIs {
+    List<PointOfInterest> closePointsOfInterest;
+    List<PointOfInterest> farPointsOfInterest;
+
+    public NearbyPOIs(List<PointOfInterest> closePointsOfInterest, List<PointOfInterest> farPointsOfInterest) {
+        this.closePointsOfInterest = closePointsOfInterest;
+        this.farPointsOfInterest = farPointsOfInterest;
+    }
+
+    public List<PointOfInterest> getClosePointsOfInterest() {
+        return closePointsOfInterest;
+    }
+
+    public void setClosePointsOfInterest(List<PointOfInterest> closePointsOfInterest) {
+        this.closePointsOfInterest = closePointsOfInterest;
+    }
+
+    public List<PointOfInterest> getFarPointOfInterest() {
+        return farPointsOfInterest;
+    }
+
+    public void setFarPointOfInterest(List<PointOfInterest> farPointOfInterest) {
+        this.farPointsOfInterest = farPointOfInterest;
+    }
+}
--- src/main/java/ClosureAnalysis/Data/Readers/POIFinder.java
+++ src/main/java/ClosureAnalysis/Data/Readers/POIFinder.java
@@ -3,6 +3,7 @@ package ClosureAnalysis.Data.Readers;
 
 import ClosureAnalysis.Calculations.DistanceCalculator;
 import ClosureAnalysis.Data.Enums.POIType;
+import ClosureAnalysis.Data.Models.NearbyPOIs;
 import ClosureAnalysis.Data.Models.PointOfInterest;
 import ClosureAnalysis.Data.Models.Stop;
 
@@ -16,12 +17,15 @@ import java.util.List;
 
 public class POIFinder {
 
+    private final double BIGRADIUS = 0.8;
+    private final double SMALLRADIUS = 0.4;
+
     private final DistanceCalculator calculator = new DistanceCalculator();
-     public  List<PointOfInterest> findPOIs(Stop stop, double radius) {
+     public NearbyPOIs findPOIs(Stop stop) {
 
         String query = "SELECT fid, buildingcategory, longitude, latitude FROM buildingsaspoints";
-        List<PointOfInterest> results = new ArrayList<>();
-
+        List<PointOfInterest> closePOIs = new ArrayList<>();
+        List<PointOfInterest> farPOIs = new ArrayList<>();
 
         try (Connection conn = DriverManager.getConnection("jdbc:sqlite:data/ClosureAnalysis/BuildingsAsPoints.sqlite");
              PreparedStatement ps = conn.prepareStatement(query);
@@ -34,9 +38,14 @@ public class POIFinder {
                 double longitude = rs.getDouble("longitude");
                 double latitude = rs.getDouble("latitude");
 
+                boolean inSmallRadius = calculator.calculateDistance(stop.getLatitude(), stop.getLongitude(), latitude, longitude) <= SMALLRADIUS;
+                boolean inBigRadius = calculator.calculateDistance(stop.getLatitude(), stop.getLongitude(), latitude, longitude) <= BIGRADIUS;
 
-                if (calculator.calculateDistance(stop.getLatitude(), stop.getLongitude(), latitude, longitude) <= radius) {
-                    results.add(new PointOfInterest(id, POIType.valueOf(type.toUpperCase()), List.of(longitude, latitude)));
+
+                if (inSmallRadius) {
+                    closePOIs.add(new PointOfInterest(id, POIType.valueOf(type.toUpperCase()), List.of(longitude, latitude)));
+                } else if (inBigRadius && !inSmallRadius) {
+                    farPOIs.add(new PointOfInterest(id, POIType.valueOf(type.toUpperCase()), List.of(longitude, latitude)));
                 }
 
 
@@ -47,14 +56,19 @@ public class POIFinder {
             e.printStackTrace();
 
         }
-        return results;
+
+        NearbyPOIs result = new NearbyPOIs(closePOIs, farPOIs);
+
+        return result;
     }
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         POIFinder finder = new POIFinder();
         Stop testStop = new Stop("1", "a", List.of(47.510571,19.056072),0,null,0,0);
 
-        List<PointOfInterest> result = finder.findPOIs(testStop, 0.4);
+        NearbyPOIs poisInRadius = finder.findPOIs(testStop);
 
-        System.out.println(result.size());
+        System.out.println(poisInRadius.getClosePointsOfInterest().size());
+        System.out.println(poisInRadius.getFarPointOfInterest().size());
+
     }
 }
