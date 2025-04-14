555eccf8803361ac99af65ba6a7400cc212b7ea9
tseknouJake <jakelockitch@gmail.com>
2025-04-14 16:06:46 +0200

    test
    Former-commit-id: 5187ba8b2ed375a624a192f71b9ff1bd3522c6b1

---
+++ src/main/java/gui/OverlayItem.java
@@ -0,0 +1,14 @@
+package gui;
+
+// note to self: we might not even need this...
+public abstract class OverlayItem {
+    int size;
+    /* implement the rest
+     * 
+     * 
+     * 
+     * 
+     * 
+     */
+
+}
