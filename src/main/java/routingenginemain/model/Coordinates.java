ef27dafb9e828995a1b1b185e81e2505c67a3c1c
Aditya Singha <adityasingha@145.127.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-04-14 17:20:18 +0200

    routing package added
    Former-commit-id: 53c76d03ec6276812ccf1b604f32a7c3bea0c35b

---
+++ src/main/java/routingenginemain/model/Coordinates.java
@@ -0,0 +1,19 @@
+package com.group.routingenginemain.model;
+
+public class Coordinates {
+    private double latitude;
+    private double longitude;
+
+    public Coordinates(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return this.latitude;
+    }
+
+    public double getLongitude() {
+        return this.longitude;
+    }
+}
