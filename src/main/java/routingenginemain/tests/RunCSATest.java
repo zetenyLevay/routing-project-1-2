04f6606d9faae08c72979159d77020392d0b26f1
Aditya Singha <adityasingha@145.127.EARLY-REGISTRATION.of.SURFnet.invalid>
2025-04-28 14:15:46 +0200

    test class updated
    Former-commit-id: d05e47bdc5241334390cecce031723ac171b4f2f
    Former-commit-id: 4a3a84025b1414c488ef1275df9e1e2e4f778105

--- src/main/java/routingenginemain/tests/RunCSATest.java
+++ src/main/java/routingenginemain/tests/RunCSATest.java
@@ -1,5 +1,29 @@
 package routingenginemain.tests;
 
-public class RunCSATest {
+import routingenginemain.engine.cache.classloader.StopsCache;
+import routingenginemain.engine.cache.masterloader.MasterLoader;
+import routingenginemain.engine.connectionscanalgorithm.CSARouteFinding;
+import routingenginemain.model.CSAAPImodel.CSAQuery;
+import routingenginemain.model.Stop;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalTime;
+
+public class RunCSATest {
+    public static void main(String[] args) {
+        MasterLoader.initAllCaches();
+        Stop departureStop = StopsCache.getStop("002133");
+        Stop arrivalStop = StopsCache.getStop("007883");
+        CSAQuery csaQuery = new CSAQuery(departureStop, arrivalStop, LocalTime.parse("08:03:00"));
+        CSARouteFinding csaRouteFinding = new CSARouteFinding(csaQuery);
+        Instant start = Instant.now();
+        csaRouteFinding.findRouteViaCSA();
+        Instant end = Instant.now();
+
+        Duration duration = Duration.between(start, end);
+        System.out.println("Route finding completed in: " + duration.toMillis() + " milliseconds");
+
+
+    }
 }
