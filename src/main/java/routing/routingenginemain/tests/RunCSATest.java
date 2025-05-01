acc03163780da276db2bc60bb12c36c92b605f41
Aditya Singha <adityasingha@mac.home>
2025-05-01 23:42:26 +0200

    blind runtime bulk testing of CSA
    Former-commit-id: 3ccf6a5dea772f8d30f6dfa5879f0d53197d918c
    Former-commit-id: 4928909498dd77a3ecfd020a0b35489cd6d112aa

--- src/main/java/routing/routingenginemain/tests/RunCSATest.java
+++ src/main/java/routing/routingenginemain/tests/RunCSATest.java
@@ -6,42 +6,36 @@ import routing.routingenginemain.engine.connectionscanalgorithm.CSARouteFinding;
 import routing.routingenginemain.model.CSAAPImodel.CSAQuery;
 import routing.routingenginemain.model.Stop;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalTime;
+import java.time.*;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class RunCSATest {
     public static void main(String[] args) {
-        Instant startLoadingCache = Instant.now();
         MasterLoader.initAllCaches();
-        Instant endLoadingCaches = Instant.now();
+        List<String> stops = StopsCache.getAllStops().stream()
+                .map(Stop::getStopID)
+                .toList();
+        Random random = new Random();
+        LocalTime depTime = LocalTime.of(8, 0);
 
-        Instant startFirstTest = Instant.now();
-        Stop departureStop = StopsCache.getStop("002133");
-        Stop arrivalStop = StopsCache.getStop("MERGED_F03810");
-        CSAQuery csaQuery = new CSAQuery(departureStop, arrivalStop, LocalTime.parse("08:03:00"));
-        CSARouteFinding csaRouteFinding = new CSARouteFinding(csaQuery);
-        csaRouteFinding.findRouteViaCSA();
-        Instant endFirstTest = Instant.now();
+        long totalTime = IntStream.range(0, 1000)
+                .mapToLong(i -> {
+                    String from = stops.get(random.nextInt(stops.size()));
+                    String to;
+                    do { to = stops.get(random.nextInt(stops.size())); }
+                    while (to.equals(from));
 
-        Instant startSecondTest = Instant.now();
-        Stop departureStop2 = StopsCache.getStop("004952");
-        Stop arrivalStop2 = StopsCache.getStop("F00179");
-        CSAQuery csaQuery2 = new CSAQuery(departureStop2, arrivalStop2, LocalTime.parse("18:43:01"));
-        CSARouteFinding csaRouteFinding2 = new CSARouteFinding(csaQuery2);
-        csaRouteFinding2.findRouteViaCSA();
-        Instant endSecondTest = Instant.now();
+                    Instant start = Instant.now();
+                    new CSARouteFinding(new CSAQuery(
+                            StopsCache.getStop(from),
+                            StopsCache.getStop(to),
+                            depTime
+                    )).findRouteViaCSA();
+                    return Duration.between(start, Instant.now()).toMillis();
+                })
+                .sum();
 
-        Duration durationLoading = Duration.between(startLoadingCache, endLoadingCaches);
-        System.out.println("Loading completed in: " + durationLoading.toMillis() + " milliseconds");
-
-        Duration durationFirstTest = Duration.between(startFirstTest, endFirstTest);
-        System.out.println("First test took " + durationFirstTest.toMillis() + "milliseconds");
-
-        Duration durationSecondTest = Duration.between(startSecondTest,endSecondTest);
-        System.out.println("Second test took " + durationSecondTest.toMillis() + "milliseconds");
-
-
-
+        System.out.printf("Average runtime: %.2f ms%n", totalTime / 1000.0);
     }
 }
