f92d19973bf534d525aa7d7570457f4ff2114646
tseknouJake <jakelockitch@gmail.com>
2025-05-05 15:27:49 +0200

    updated gutugnore again
    Former-commit-id: 8ac79db866c56345c2244941f6983abee9002607
    Former-commit-id: 0cc4176fb0b3213ee601a0c50d5da11195bf2c33

---
+++ src/main/java/RoutingEngineModels/RouteStep.java
@@ -0,0 +1,31 @@
+package RoutingEngineModels;
+
+public class RouteStep {
+    // {Mode of transpotr, startCoord, endCoord, Time}
+    private String modeOfTransport;
+    private Coordinate startCoord;
+    private Coordinate endCoord;
+    private double time;
+
+    public RouteStep(String modeOfTransport, Coordinate startCoord, Coordinate endCoord, double time) {
+        this.modeOfTransport = modeOfTransport;
+        this.startCoord = startCoord;
+        this.endCoord = endCoord;
+        this.time = time;
+    }
+    public String getModeOfTransport() {
+        return modeOfTransport;
+    }
+    public Coordinate getStartCoord() {
+        return startCoord;
+    }   
+    public Coordinate getEndCoord() {
+        return endCoord;
+    }
+    public double getTime() {
+        return time;
+    }
+    public void setModeOfTransport(String modeOfTransport) {
+        this.modeOfTransport = modeOfTransport;
+    }
+}
