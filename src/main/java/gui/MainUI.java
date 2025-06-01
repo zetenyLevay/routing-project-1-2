469ac83dab3bbbb5d6c80eb394e22fb9dc13dd09
tseknouJake <jakelockitch@gmail.com>
2025-06-01 16:37:42 +0200

    got DB
    Former-commit-id: 04cd588321b419468f3afc848ea7ff17b1756a80
    Former-commit-id: 31311452775aef06f68481a92690108b1c6d47d4

--- src/main/java/gui/MainUI.java
+++ src/main/java/gui/MainUI.java
@@ -2,12 +2,17 @@ package gui;
 
 import java.io.IOException;
 
+import parsers.RequestHandler;
+
 public class MainUI {
 
     public static void main(String[] args) throws IOException {
         //uncomment at some point
-              MapUI.create();
-        // RequestHandler rh = new RequestHandler();
-        // rh.run();
+            //   MapUI.create();
+            //{"load":"/Users/jakelockitch/Downloads/budapest_gtfs.zip"}
+            //{"load":"budapest_gtfs.zip"}
+            //NOTE: you have to be explicit about the path to the GTFS file
+        RequestHandler rh = new RequestHandler();
+        rh.run(); 
     }
 }
