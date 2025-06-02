484c2f4c6c413e689549c4ecc8f1be6d3b705f86
Nurali Kabiyev <n.kabiyev@student.maastrichtuniversity.nl>
2025-06-02 16:02:41 +0200

    Separated mapUI into many classes
    Former-commit-id: 309eb226143e7ed78a46203cac66bb242af925d8
    Former-commit-id: 8a8e077318bf07d4104466c6290c0eb2768856e0

--- src/main/java/gui/GeoPosition.java
+++ src/main/java/gui/GeoPosition.java
@@ -1,5 +1,20 @@
 package gui;
 
 public class GeoPosition {
+    private final double latitude;
+    private final double longitude;
 
+    public GeoPosition(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
 }
+
