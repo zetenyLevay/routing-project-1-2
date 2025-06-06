6a4c519ec2e4df164c6d8e0b307dd0352824247f
Liviu <l.dan@maastrichtuniversity.nl>
2025-06-06 14:54:55 +0200

    ignored mapUI create so we can test on tonys server
    Former-commit-id: aa8661c841300f40f7bc65dd43686515dd66dbc2
    Former-commit-id: a0403c9b8f4d7c63b7ccfff041ff181d36a9988a

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         RequestHandler requestHandler = new RequestHandler();
 
-        MapUI.create();
+        // MapUI.create();
         requestHandler.run();
 
     }
